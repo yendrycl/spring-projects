3b07163e1a055b38e8734ba34777f9bf1340b2ae
Yendry <yendrycastanera@gmail.com>
2021-10-06 12:19:00 -0400

    first commit

---
+++ democrudmysql/src/main/java/com/example/democrudmysql/DemocrudmysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.democrudmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocrudmysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemocrudmysqlApplication.class, args);
+    }
+
+}
---
+++ democrudmysql/src/test/java/com/example/democrudmysql/DemocrudmysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democrudmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemocrudmysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
